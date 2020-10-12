3ed7ed070a2bb2ebb972b0befc54182aa39c111d
MoreCoke <seed45699@gmail.com>
2020-10-12 16:43:40 +0800

    Fix mobx issue for ListHeaderComponent and ListFooterComponent
    因為 ListHeaderComponent 和 ListFooterComponent 中有用到 observable 的變數， todoViewModel.addText 和 todoViewModel.loading 所以要再包 observer，這樣資料才會更新

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -25,82 +25,73 @@ class TodoList extends React.Component {
     todoViewModel.appDie();
   }
 
-  renderListHeader = () => {
-    return (
-      <View style={styles.headerView}>
-        <View style={styles.sectionContainer}>
-          <TextInput
-            style={{
-              height: 40,
-              borderColor: 'gray',
-              borderWidth: 1,
-            }}
-            value={todoViewModel.addText}
-            autoCapitalize="none"
-            autoCorrect={false}
-            onChangeText={(text) => todoViewModel.updateAddInputValue(text)}
-          />
-          <Button color="black" title="新增" onPress={todoViewModel.addTodo} />
-        </View>
-        <View
-          style={[
-            {
-              flexDirection: 'row',
-              justifyContent: 'center',
-            },
-            styles.sectionContainer,
-          ]}>
-          <Button color="black" title="全部" onPress={todoViewModel.allTodo} />
-          <Button
-            color="black"
-            title="已完成"
-            onPress={todoViewModel.allDoneTodo}
-          />
-        </View>
+  renderListHeader = observer(() => (
+    <View style={styles.headerView}>
+      <View style={styles.sectionContainer}>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+          }}
+          value={todoViewModel.addText}
+          autoCapitalize="none"
+          autoCorrect={false}
+          onChangeText={(text) => todoViewModel.updateAddInputValue(text)}
+        />
+        <Button color="black" title="新增" onPress={todoViewModel.addTodo} />
       </View>
-    );
-  };
+      <View
+        style={[
+          {
+            flexDirection: 'row',
+            justifyContent: 'center',
+          },
+          styles.sectionContainer,
+        ]}>
+        <Button color="black" title="全部" onPress={todoViewModel.allTodo} />
+        <Button
+          color="black"
+          title="已完成"
+          onPress={todoViewModel.allDoneTodo}
+        />
+      </View>
+    </View>
+  ));
 
-  renderListFooter = () => {
-    return (
-      <>
-        {todoViewModel.loading && (
-          <View style={[styles.sectionContainer, styles.sectionCenter]}>
-            <Image
-              style={styles.loading}
-              source={{
-                uri: 'https://reactnative.dev/img/tiny_logo.png',
-                // "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' style='margin: auto; display: block; shape-rendering: auto;' width='100px' height='100px' viewBox='0 0 100 100' preserveAspectRatio='xMidYMid'%3E%3Ccircle cx='50' cy='50' r='32' stroke-width='8' stroke='%231d3f72' stroke-dasharray='50.26548245743669 50.26548245743669' fill='none' stroke-linecap='round'%3E%3CanimateTransform attributeName='transform' type='rotate' repeatCount='indefinite' dur='1s' keyTimes='0;1' values='0 50 50;360 50 50'%3E%3C/animateTransform%3E%3C/circle%3E%3C/svg%3E",
-              }}
-            />
-          </View>
-        )}
-        <View
-          style={[
-            {
-              flexDirection: 'row',
-              justifyContent: 'center',
-            },
-            styles.sectionContainer,
-          ]}>
-          <Button
-            color="black"
-            title="載入更多"
-            onPress={todoViewModel.query}
+  renderListFooter = observer(() => (
+    <>
+      {todoViewModel.loading && (
+        <View style={[styles.sectionContainer, styles.sectionCenter]}>
+          <Image
+            style={styles.loading}
+            source={{
+              uri: 'https://reactnative.dev/img/tiny_logo.png',
+              // "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' style='margin: auto; display: block; shape-rendering: auto;' width='100px' height='100px' viewBox='0 0 100 100' preserveAspectRatio='xMidYMid'%3E%3Ccircle cx='50' cy='50' r='32' stroke-width='8' stroke='%231d3f72' stroke-dasharray='50.26548245743669 50.26548245743669' fill='none' stroke-linecap='round'%3E%3CanimateTransform attributeName='transform' type='rotate' repeatCount='indefinite' dur='1s' keyTimes='0;1' values='0 50 50;360 50 50'%3E%3C/animateTransform%3E%3C/circle%3E%3C/svg%3E",
+            }}
           />
         </View>
-      </>
-    );
-  };
+      )}
+      <View
+        style={[
+          {
+            flexDirection: 'row',
+            justifyContent: 'center',
+          },
+          styles.sectionContainer,
+        ]}>
+        <Button color="black" title="載入更多" onPress={todoViewModel.query} />
+      </View>
+    </>
+  ));
 
   render() {
     return (
       <FlatList
         style={styles.sectionContainer}
         data={todoViewModel.todoItems}
-        // 加 () 的差別是什麼, 目前得知加了 addText 就會更新正常
-        ListHeaderComponent={this.renderListHeader()}
-        ListFooterComponent={this.renderListFooter()}
+        ListHeaderComponent={this.renderListHeader}
+        ListFooterComponent={this.renderListFooter}
         stickyHeaderIndices={[0]}
         keyExtractor={(element) => element['id'].toString()}
         renderItem={({item}) => (
