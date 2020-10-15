a9d3b1e1376cbe0bb9292f50e5a91a5112604f4e
MoreCoke <seed45699@gmail.com>
2020-10-15 10:47:22 +0800

    Feat react navigation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {observer} from 'mobx-react';
 import {makeObservable, observable, runInAction} from 'mobx';
-import {SafeAreaView, StyleSheet, View, StatusBar} from 'react-native';
+import {SafeAreaView, StyleSheet, View, StatusBar, Button} from 'react-native';
 // import AsyncStorage from '@react-native-community/async-storage';
 
+import SplashScreen from 'react-native-splash-screen';
+import 'react-native-gesture-handler';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Stores from './stores';
 import TodoList from './components/TodoList';
 
+const Tab = createBottomTabNavigator();
+
 @observer
-class App extends React.Component {
+class Home extends React.Component {
   @observable isHydrateFinished = false;
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     makeObservable(this);
   }
 
@@ -26,17 +32,46 @@ class App extends React.Component {
   }
 
   render() {
+    console.log('the props from react navigation', this.props);
     return (
       <>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView>
           <View style={styles.body}>
             {this.isHydrateFinished && <TodoList />}
+            <Button
+              title="切換頁面"
+              onPress={() => this.props.navigation.navigate('Test')}
+            />
           </View>
         </SafeAreaView>
       </>
     );
   }
+}
+
+function Test({navigation}) {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View>
+        <Button title="回上頁" onPress={() => navigation.goBack()} />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Test" component={Test} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
