66193ef13257974a592837574495cbe307afb908
MoreCoke <seed45699@gmail.com>
2020-10-21 11:56:06 +0800

    Refactor layout and function because of Edit page

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,5 +1,14 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, TextInput, Button} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+import {useNavigation, Link} from '@react-navigation/native';
 import CheckBox from '@react-native-community/checkbox';
 import {observer} from 'mobx-react';
 
@@ -20,12 +29,34 @@ class TodoItem extends React.Component {
         markTodo,
       },
       deleteTodo,
+      navigation,
     } = this.props;
     return (
       <View>
-        <CheckBox value={isCompleted} onValueChange={markTodo} />
-        <Text>{text}</Text>
-        {isEdited && (
+        <View style={styles.titleLayout}>
+          <CheckBox
+            style={{marginRight: 16}}
+            value={isCompleted}
+            onValueChange={markTodo}
+          />
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate('Edit', {
+                id,
+                text,
+                isEdited,
+                updateEditInputValue,
+                editTodo,
+                deleteTodo,
+              })
+            }>
+            <Text style={styles.title}>{text}</Text>
+          </TouchableOpacity>
+          {/* <Link style={styles.title} to={`/Edit/${id}`}>
+            {text}
+          </Link> */}
+        </View>
+        {false && isEdited && (
           <TextInput
             style={{height: 40, borderColor: 'gray', borderWidth: 1}}
             defaultValue={text}
@@ -35,21 +66,38 @@ class TodoItem extends React.Component {
             className={isEdited ? '' : 'none'}
           />
         )}
-        <Button
-          color="black"
-          title={isEdited ? '完成編輯' : '編輯'}
-          onPress={editTodo}
-        />
-        <Button
-          color="black"
-          title="刪除"
-          onPress={() => {
-            deleteTodo(id);
-          }}
-        />
+        {false && (
+          <Button
+            color="black"
+            title={isEdited ? '完成編輯' : '編輯'}
+            onPress={editTodo}
+          />
+        )}
+        {false && (
+          <Button
+            color="black"
+            title="刪除"
+            onPress={() => {
+              deleteTodo(id);
+            }}
+          />
+        )}
       </View>
     );
   }
 }
 
-export default TodoItem;
+const styles = StyleSheet.create({
+  titleLayout: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  title: {
+    fontSize: 30,
+  },
+});
+
+export default function (props) {
+  const navigation = useNavigation();
+  return <TodoItem {...props} navigation={navigation} />;
+}
